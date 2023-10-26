e340144d2b180b0694e53edb64b896f81e4acb5b
Victor <vdr8@gcloud.ua.es>
2023-10-26 12:16:39 +0200

    Añadidos archivos de Prac2 y modificado index.html de Prac1

---
+++ Prac2/jvs/cuestion.js
@@ -0,0 +1,62 @@
+let cuestionarioCount = 1;  // Variable global para contar los cuestionarios
+
+function addCuestionario(event) {
+    event.preventDefault(); // Prevener el comportamiento por defecto del formulario
+
+    // Acceder a los datos del formulario
+    const form = event.target.closest(".formulario");
+    const titulo = form.querySelector("[name='tema']").value;
+    const imgUrl = form.querySelector("[name='imagen']").value;
+
+    // Comprobar que los campos no estén vacíos
+    if (!titulo || !imgUrl) {
+        alert("Todos los campos son obligatorios.");
+        return;
+    }
+
+    // Llama a la función para agregar el cuestionario
+    agregarCuestionario(titulo, imgUrl);
+    // Incrementar el contador
+    cuestionarioCount++;
+
+    // Limpiar los campos del formulario
+    form.querySelector("[name='tema']").value = "";
+    form.querySelector("[name='imagen']").value = "";
+}
+
+
+function agregarCuestionario(titulo, imgUrl) {
+    // Crear el nuevo elemento section
+    const nuevoCuestionario = document.createElement('section');
+
+    // Establecer el contenido del nuevo cuestionario
+    nuevoCuestionario.innerHTML = `
+        <section id= tolowercase(${titulo})>
+        <h2><img src="${imgUrl}" alt="${titulo}"> Cuestionario sobre ${titulo}</h2>
+        <!-- Puedes agregar más bloques de preguntas aquí si lo necesitas -->
+    `;
+
+    // Añadir el nuevo cuestionario al final del section
+    const footer = document.querySelector('footer');
+    document.body.insertBefore(nuevoCuestionario, footer);
+
+    // Agregar el formulario de creación de preguntas
+    addFormPregunta(nuevoCuestionario);
+
+    // Obtiene el valor del tema
+    let tema = document.querySelector('input[name="tema"]').value;
+
+    // Crear un nuevo elemento <ul> para el título del cuestionario
+    let ul = document.createElement('ul');
+    ul.textContent = tema;
+
+    // Obtiene la etiqueta <nav>
+    let nav = document.querySelector('nav');
+
+    // Agrega el nuevo <ul> al final de la etiqueta <nav>
+    nav.appendChild(ul);
+}
+
+
+
+
---
+++ Prac2/jvs/init.js
@@ -0,0 +1,63 @@
+
+// Función para agregar el icono de eliminar a un bloque
+function addCruz(bloqueNode) {
+    // Crear el nuevo nodo con el ícono
+    var cruz = document.createElement('div');
+    cruz.className = 'borra';
+    cruz.innerHTML = '&#x2612;'; // Código Unicode para ☒
+    cruz.style.cursor = 'pointer'; // Hacer que el cursor cambie a un puntero al pasar sobre la cruz
+
+    // Añadir el evento de clic al ícono
+    cruz.addEventListener('click', function(e) {
+      borraPregunta(bloqueNode);
+    });  
+    // Añadir este nodo al principio del bloque (como primer hijo)
+    bloqueNode.insertBefore(cruz, bloqueNode.firstChild);
+}
+
+
+// Función para eliminar una pregunta y su cuestionario si es necesario
+function borraPregunta(bloqueNode) {
+    // Obtener el ancestro con selector .bloque
+    var bloque = bloqueNode.closest('.bloque');
+
+    // Verificar si se encontró un bloque
+    if (bloque) {
+      // Eliminar el bloque del DOM
+      bloque.remove();
+
+      // Verificar si no hay más preguntas en el cuestionario
+      var preguntas = document.querySelectorAll('.bloque');
+      if (preguntas.length === 0) {
+        // Si no hay preguntas, eliminar el cuestionario del DOM
+        var cuestionario = document.querySelector('.cuestionario');
+        cuestionario.remove();
+
+        // Eliminar la entrada en el índice al principio de la página
+        var indice = document.querySelector('.indice');
+        indice.remove();
+      }
+    }
+
+}
+
+//Agregar la cruz de borrado a todos los bloques cuando la página se carga
+document.addEventListener('DOMContentLoaded', function() {
+  
+    // Obtener todos los elementos con clase .bloque y agregar la cruz de borrado a cada uno
+    var bloques = document.querySelectorAll('.bloque');
+    bloques.forEach(function(bloque) {
+      addCruz(bloque);
+    });
+});
+
+// Llamar a addFormPregunta cuando la página se carga
+window.addEventListener('DOMContentLoaded', (event) => {
+    const sections = document.querySelectorAll('section');
+    sections.forEach(sectionNode => addFormPregunta(sectionNode));
+});
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const button = document.querySelector('input[name="crea"]');
+    button.addEventListener('click',addCuestionario);
+});
